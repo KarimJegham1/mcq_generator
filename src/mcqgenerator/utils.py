844598149e7bb7d62ec6b0de72c7d770c1c4b1ff
KarimJegham1 <Karimjegham3@gmail.com>
2024-10-06 14:19:43 +0200

    updated

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -23,28 +23,3 @@ def read_file(file):
         raise Exception(
             "unsupported file format only pdf and text file suppoted"
             )
-
-def get_table_data(report_str):
-    try:
-        # convert the report from a str to dict
-        report_dict=json.loads(report_str)
-        report_table_data=[]
-        
-        # iterate over the repport dictionary and extract the required information
-        for key,value in report_dict.items():
-            mcq=value["mcq"]
-            options=" || ".join(
-                [
-                    f"{option}-> {option_value}" for option, option_value in value["options"].items()
-                 
-                 ]
-            )
-            
-            correct=value["correct"]
-            quiz_table_data.append({"MCQ": mcq,"Choices": options, "Correct": correct})
-        
-        return quiz_table_data
-        
-    except Exception as e:
-        traceback.print_exception(type(e), e, e.__traceback__)
-        return False
